86dc16e3db4284630d20c3fec363be8a3572c92b
masih jazayeri <masih1376@gmail.com>
2020-12-28 23:21:16 +0330

    fix ServiceRepository.php

--- src/Entities/ServiceRepository.php
+++ src/Entities/ServiceRepository.php
@@ -14,13 +14,13 @@ class ServiceRepository
 
     public function setServices($value)
     {
-        Cache::put($this->getServicesCacheKey(), $value, (new Presence())->getTTL());
+        Cache::put(static::SERVICE_KEY, $value, (new Presence())->getTTL());
         return $this;
     }
 
     public function getServices()
     {
-        return Cache::get($this->getServicesCacheKey(), function () {
+        return Cache::get(static::SERVICE_KEY, function () {
             $this->setServices(\AngusDV\DiscoveryClient\Facades\ServiceDiscoverer::discover()->getServices());
             return $this->getServices();
         });
