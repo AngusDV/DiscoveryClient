ad0ed02a1a5a3b556890ff6cfd61e34b3a0dbe1b
masih jazayeri <masih1376@gmail.com>
2020-12-28 23:09:41 +0330

    add default value in config.php

--- src/config.php
+++ src/config.php
@@ -1,12 +1,12 @@
 <?php
 
 return [
-    'SERVICE_DISCOVERY_ADDRESS'=> env('SERVICE_DISCOVERY_ADDRESS'),
-    'SERVICE_NAME'=> env('SERVICE_NAME'),
-    'SERVICE_HOST'=> env('SERVICE_HOST'),
-    'SERVICE_PORT'=> env('SERVICE_PORT'),
-    'SERVICE_TTL'=> env('SERVICE_TTL'),
-    'response_model'=> \AngusDV\DiscoveryClient\Entities\PresenceResponse::class,
-    'discoverer_model'=> \AngusDV\DiscoveryClient\Entities\ServiceDiscoverer::class,
-    'service_response_model'=> \AngusDV\DiscoveryClient\Entities\ServiceResponse::class,
+    'SERVICE_DISCOVERY_ADDRESS' => env('SERVICE_DISCOVERY_ADDRESS', '127.0.0.1'),
+    'SERVICE_NAME' => env('SERVICE_NAME', env('APP_NAME')),
+    'SERVICE_HOST' => env('SERVICE_HOST', env('APP_URL')),
+    'SERVICE_PORT' => env('SERVICE_PORT', '80'),
+    'SERVICE_TTL' => env('SERVICE_TTL', 300),
+    'response_model' => \AngusDV\DiscoveryClient\Entities\PresenceResponse::class,
+    'discoverer_model' => \AngusDV\DiscoveryClient\Entities\ServiceDiscoverer::class,
+    'service_response_model' => \AngusDV\DiscoveryClient\Entities\ServiceResponse::class,
 ];
