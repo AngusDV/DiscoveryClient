bd3dfdc2aaef8c091c79984a5ce75dcea391cb8c
hassan mousavi <hsmsender1370@gmail.com>
2020-12-28 16:37:33 +0330

    fix problem in findOrFail of service repository

--- src/Entities/ServiceRepository.php
+++ src/Entities/ServiceRepository.php
@@ -43,7 +43,7 @@ class ServiceRepository
 
     public function findOrFail($name)
     {
-        $service= $this->getServices()->where('name',$name)->first() || $this->forceGetServices();
+        $service= $this->getServices()->where('name',$name)->first() ?: $this->forceGetServices()->where('name',$name)->first();
         throw_if(is_null($service), new Exception("service is not available"));
         return $service;
     }
